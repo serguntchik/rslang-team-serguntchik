24521e4e518663ce112e8093d8070f65d24ab886
Olga <klokova.plus@gmail.com>
2022-02-13 11:03:47 +0100

    fix select element

--- src/routes/Sprint/Sprint.tsx
+++ src/routes/Sprint/Sprint.tsx
@@ -1,34 +1,38 @@
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import React from 'react';
-import { BasicSelect } from './BasicSelect';
 import './Sprint.css';
 
-const select = BasicSelect();
-const count = 0;
-const timer = 0;
+export const Sprint: React.FC = () => {
+    const [level, setLevel] = React.useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+        setLevel(event.target.value as string);
+    };
 
-export const Sprint: React.FC = () => (
-    <div className="sprint">
-        <h1 className="h1">Спринт</h1>
-        <div>
-            «Спринт» - это тренировка для повторения заученных слов из вашего словаря.
-            <ul>
-                <li>Используйте мышь, чтобы выбрать</li>
-            </ul>
-            <div className="sprint">
-                {select}
-                <button className="btns btn-start" type="button">
-                    НАЧАТЬ
-                </button>
+    return (
+        <div className="sprint">
+            <h1 className="h1">Спринт</h1>
+            <div>
+                «Спринт» - это тренировка для повторения заученных слов из вашего словаря.
+                <ul>
+                    <li>Используйте мышь, чтобы выбрать</li>
+                </ul>
+                <div className="sprint">
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={level}
+                        label="Level"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                    <button className="btns btn-start" type="button">
+                        НАЧАТЬ
+                    </button>
+                </div>
             </div>
         </div>
-        <div className="sprint card">
-            <h1 className="h1">
-                Текущий результат -
-                {count}
-            </h1>
-            <div className="card_body">
-                <div className="timer">{timer}</div>
-            </div>
-        </div>
-    </div>
-);
+    );
+};
