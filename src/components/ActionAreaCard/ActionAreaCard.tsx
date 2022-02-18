e263b4038c91f4d8df58609230921e91923f5684
Olga <klokova.plus@gmail.com>
2022-02-18 22:52:58 +0100

    fix: add photo, links git

--- src/components/ActionAreaCard/ActionAreaCard.tsx
+++ src/components/ActionAreaCard/ActionAreaCard.tsx
@@ -4,22 +4,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { StringDecoder } from 'string_decoder';
 
 export interface TeamCard {
     image: string;
     about: string;
     name: string;
+    git: string;
 }
 
 export const ActionAreaCard: React.FC<TeamCard> = (props: TeamCard) => {
-    const { image, about, name } = props;
+    const {
+        image, about, name, git,
+    } = props;
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
                 <CardMedia component="img" height="140" image={image} alt="photo" />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        {name}
+                        <a href={git} target="_blank" rel="noreferrer">
+                            {name}
+                            <img src="./logo_git.svg" alt="logo_git" />
+                        </a>
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         {about}
--- src/routes/Team/Team.tsx
+++ src/routes/Team/Team.tsx
@@ -5,27 +5,31 @@ import './Team.css';
 const infoTeam = [
     {
         id: '1',
-        image: '/avaredcat.png',
-        about: 'Тимлид',
-        name: 'Сергей',
+        image: '/sergey.jpg',
+        about: 'Тимлид, разработал архитектуру приложения и руководил командой.',
+        name: 'Сергей ',
+        git: 'https://github.com/Olga-plus',
     },
     {
         id: '2',
         image: '/viktor.jpg',
-        about: 'разработчик',
-        name: 'Виктор',
+        about: 'Настроил аутентификацию, разработал игру "Аудиовызов".',
+        name: 'Виктор ',
+        git: 'https://github.com/Olga-plus',
     },
     {
         id: '3',
         image: '/avapanda.png',
-        about: 'разработчик',
-        name: 'Любомир',
+        about: 'Разработал страницы словаря и учебника.',
+        name: 'Любомир ',
+        git: 'https://github.com/Olga-plus',
     },
     {
         id: '4',
         image: '/olga.jpg',
-        about: 'разработчик',
-        name: 'Ольга',
+        about: 'Сделала главную страницу, разработала игру "Спринт"',
+        name: ' Ольга ',
+        git: 'https://github.com/Olga-plus',
     },
 ];
 
@@ -34,7 +38,7 @@ export const Team: React.FC = () => (
         <h1>Наша команда</h1>
         <div className="wrapper__team_page">
             {infoTeam.map((item) => (
-                <ActionAreaCard image={item.image} about={item.about} name={item.name} />
+                <ActionAreaCard image={item.image} about={item.about} name={item.name} git={item.git} />
             ))}
         </div>
     </div>
