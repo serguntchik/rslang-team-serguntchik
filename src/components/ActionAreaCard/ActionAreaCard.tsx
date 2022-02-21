3f10bc886befbd52b330bc874d6705f8a48bc3e9
Sergey Volkov <volkov.s@n-t.io>
2022-02-21 22:25:16 +0300

    feat: localize main page

--- src/components/ActionAreaCard/ActionAreaCard.tsx
+++ src/components/ActionAreaCard/ActionAreaCard.tsx
@@ -4,22 +4,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { StringDecoder } from 'string_decoder';
 
 export interface TeamCard {
     image: string;
     about: string;
     name: string;
+    git: string;
 }
 
 export const ActionAreaCard: React.FC<TeamCard> = (props: TeamCard) => {
-    const { image, about, name } = props;
+    const {
+        image, about, name, git,
+    } = props;
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 150 }}>
             <CardActionArea>
                 <CardMedia component="img" height="140" image={image} alt="photo" />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        {name}
+                        <a href={git} target="_blank" rel="noreferrer">
+                            <img src="/logo_git_violet.svg" alt="git-logo" className="git_logo_teams" />
+                            {name}
+                        </a>
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         {about}
--- src/routes/MainPage/MainPage.tsx
+++ src/routes/MainPage/MainPage.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import Button from '@mui/material/Button';
+
 import './MainPage.css';
 
 export const MainPage: React.FC = () => (
@@ -17,28 +19,23 @@ export const MainPage: React.FC = () => (
             <div className="wrapper_slider">
                 <Carousel>
                     <div className="wrapper__slader_container">
-                        <h1 className="h1">Our team</h1>
-                        <span className="text__about">
-                            We are Sergey, Victor, Lubomir and Olga - a team of young professionals in the Rolling
-                            Scopes School React course, we offer you an application for learning English.
-                        </span>
-                        <Link to="/team">
-                            <button className="btns btn_color1" type="button">
-                                more
-                            </button>
-                        </Link>
-                    </div>
-                    <div className="wrapper__slader_container">
                         <h1 className="h1">RS Lang</h1>
                         <span className="text__about">
-                            Now learning English is easy and fun! Play mini-games and learn to memorize words. The
-                            dictionary contains all the words that were previously encountered in games. Repeat them
-                            every day to consolidate the result.
+                            Изучай английский язык легко и увлекательно! Играя в мини-игры учи и запоминай слова. В
+                            словаре находятся все слова, которые встречались в играх раньше. Повторяйте их каждый день
+                            для закрепления результата.
+                        </span>
+                    </div>
+                    <div className="wrapper__slader_container">
+                        <h1 className="h1">Наша команда</h1>
+                        <span className="text__about">
+                            Мы Сергей, Виктор, Любомир и Ольга - команда js разработчиков в рамках курса Rolling Scopes
+                            school представляем вам приложение для изучения английского языка.
                         </span>
                         <Link to="/team">
-                            <button className="btns btn_color1" type="button">
-                                more
-                            </button>
+                            <Button size="medium" variant="outlined">
+                                Узнать больше
+                            </Button>
                         </Link>
                     </div>
                 </Carousel>
